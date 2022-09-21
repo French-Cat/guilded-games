d5edf290e83c057f5fb7c36630c2afa7ea62272e
FRCat.JS <89495053+French-Cat@users.noreply.github.com>
2022-09-21 00:11:05 +0000

    test

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 
+const { Client } = require("guilded.js");
+
+const client = new Client({ token: "TOKEN_HERE" });
+client.on("ready", () => console.log("Bot is successfully logged in"));
+client.on("messageCreated", (message) => {
+    if (message.content === "test") {
+        return message.send("poggers!");
+    }
+});
+client.login()
